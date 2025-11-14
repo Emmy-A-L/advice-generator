b93d27ddfd773dcd64190b723af342d1536acedd
Emmy-A-L <elot833@gmail.com>
2025-11-14 12:13:03 +0100

    adjusted the logo spacing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-18 bg-transparent flex px-8 py-16 md:py-8 items-center">
+    <div className="fixed top-0 left-0 w-full h-18 bg-transparent flex px-8 py-8 md:py-8 items-center">
       <img
         src="/advice-generator-high-resolution-logo-transparent.png"
         alt="Advice Generator Logo"
