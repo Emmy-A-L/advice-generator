86fab376f8e59e0f61b29c70d66c65704426cb67
Emmy-A-L <elot833@gmail.com>
2026-05-14 15:03:50 +0100

    feat: add navigation bar component and site icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,10 @@ import { FaHeart } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-18 bg-transparent flex px-8 py-8 md:py-8 items-center z-50">
+    <div className="fixed w-auto bottom-2 left-4 right-4 rounded-4xl bg-slate-800/40 backdrop-blur-xl border border-white/5 md:fixed md:top-0 md:left-0 md:h-18 md:bg-transparent md:backdrop-blur-none md:border-0 flex px-8 py-8 md:py-8 items-center z-50">
       <Link to="/" className="">
         <img
-          src="/advice-generator-high-resolution-logo-transparent.png"
+          src="/icon.png"
           alt="Advice Generator Logo"
           className="w-12 h-12"
         />
@@ -14,10 +14,10 @@ const Navbar = () => {
 
       <Link
         to="/favourites"
-        className="absolute right-6 top-1/2 -translate-y-1/2 flex items-center gap-2 text-slate-400 hover:text-[#52ffaa] transition-colors duration-300 group"
+        className="absolute w-12 h-12 right-6 top-1/2 -translate-y-1/2 flex items-center gap-2 text-slate-400 hover:text-[#52ffaa] transition-colors duration-300 group"
         aria-label="Favourites"
       >
-        <FaHeart className="text-lg group-hover:drop-shadow-[0_0_8px_rgba(82,255,170,0.5)] transition-all duration-300" />
+        <FaHeart className="text-4xl group-hover:drop-shadow-[0_0_8px_rgba(82,255,170,0.5)] transition-all duration-300" />
         <span className="text-sm font-semibold hidden sm:inline">Favourites</span>
       </Link>
     </div>
